89fd533115343791fb4d3382801aa98657791f41
Philip Chan <philip.chan92@icloud.com>
2019-06-23 20:21:12 +0200

    fixed albumphoto and playlistphoto errors, added playlist-user association

--- app/models/artist_album.rb
+++ app/models/artist_album.rb
@@ -1,5 +1,6 @@
 class ArtistAlbum < ApplicationRecord
   belongs_to :artist
   has_many :artist_songs
+  has_many :album_photos
   belongs_to :genre
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,6 @@
 class Playlist < ApplicationRecord
   has_many :user_song
   has_many :playlist_likes
+  has_many :playlist_photos
+  belongs_to :user
 end
