358bef1073b17cebd6b9430d6d5294acb0c9af25
jackson huang <jacksonh201325@gmail.com>
2019-07-14 12:12:35 -0400

    Add Follows to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_201740) do
+ActiveRecord::Schema.define(version: 2019_07_14_161202) do
 
   create_table "artist_albums", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2019_06_16_201740) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_artists_on_email", unique: true
     t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|
