fd59f0d25ab5bb9aabc0dc49db4a3f6f75379efe
Jackson Huang <jackson@oktana.io>
2019-07-18 17:16:49 -0400

    Create follows table and added fields to tables

--- db/migrate/20190718203020_create_posts.rb
+++ db/migrate/20190718203020_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.text :description
-      t.reference :user
+      t.references :user
 
       t.timestamps
     end
